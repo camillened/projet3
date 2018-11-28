32cfc010615db0525bc47d7039fc9c65cc7e574a
camille <camille.nedjar@gmail.com>
2018-11-28 13:29:35 +0100

    Creation template

---
+++ template.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet" />
+    <title>Billet simple pour l'Alaska - Jean Forteroche</title>
+  </head>
+  <body>
+    <div id="all">
+      <header id="header">
+        <a href="index.php"><h1 id="titreBlog">Billet simple pour l'Alaska</h1></a>
+        <h2>Par Jean Forteroche</h2>
+      </header><!-- #header -->
+
+      <div id="content">
+       
+
+      </div> <!-- #content -->
+
+      <footer id="footer">
+        <p><a href="   lien Connexion   ">Connexion</a></p>
+      </footer> <!-- #footer -->
+    </div> <!-- #all -->
+  </body>
+</html>
