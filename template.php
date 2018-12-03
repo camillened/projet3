34bedf39b5a2a2886ac3ebc02c104545a50c360a
camille <camille.nedjar@gmail.com>
2018-12-03 23:47:39 +0100

    template : ajout des variables $content et $title

--- template.php
+++ template.php
@@ -3,18 +3,16 @@
   <head>
     <meta charset="UTF-8" />
     <link href="bootstrap/css/bootstrap.css" rel="stylesheet" />
-    <title>Billet simple pour l'Alaska - Jean Forteroche</title>
+    <title><?= $title ?></title>
   </head>
   <body>
     <div id="all">
       <header id="header">
         <a href="index.php"><h1 id="titreBlog">Billet simple pour l'Alaska</h1></a>
-        <h2>Par Jean Forteroche</h2>
       </header><!-- #header -->
 
       <div id="content">
-       
-
+        <?= $content ?>
       </div> <!-- #content -->
 
       <footer id="footer">
