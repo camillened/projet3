0f2ad7ecc1601276c054e557030ae4a114c6b845
camille <camille.nedjar@gmail.com>
2018-12-07 12:34:45 +0100

    archive des essais - à garder au cas où

---
+++ archive essais/erreurview.php
@@ -0,0 +1,7 @@
+<?php $title = ' "Billet simple pour l\'Alaska"'; ?>
+
+<?php ob_start() ?>
+<p>Une erreur est survenue : <?= $msgErreur ?></p>
+<?php $content = ob_get_clean(); ?>
+
+<?php require 'template.php'; ?>
