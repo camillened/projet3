2c8788d7008741fc6fc7328beb519d87e35215d5
camille <camille.nedjar@gmail.com>
2018-12-09 21:15:03 +0100

    Tri archives

--- archive essais/erreurview.php
+++
@@ -1,7 +0,0 @@
-<?php $title = ' "Billet simple pour l\'Alaska"'; ?>
-
-<?php ob_start() ?>
-<p>Une erreur est survenue : <?= $msgErreur ?></p>
-<?php $content = ob_get_clean(); ?>
-
-<?php require 'template.php'; ?>
