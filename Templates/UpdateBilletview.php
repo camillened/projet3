50813c8f4959ed298c81a08fd17c6b7d7a216853
camille <camille.nedjar@gmail.com>
2019-01-22 17:24:57 +0100

    Résolution du problème de mise à jour billet

--- Templates/UpdateBilletview.php
+++ Templates/UpdateBilletview.php
@@ -21,7 +21,7 @@ use App\Modele;
     <article>
         <form method="post" action="<?= "index.php?action=saveupdatebillet&id=" . $billet['billet_id'] ?>">
             <input name="title" type="text" placeholder="Titre du billet" value="<?=$billet['billet_title']?>" size="30" maxlength="150" required /> 
-            <textarea id="mytextarea" name="content" value="<?=$billet['billet_content']?>" required></textarea>
+            <textarea id="mytextarea" name="content" required><?=$billet['billet_content']?></textarea>
             <input type="hidden" name="id" value="<?=$billet['billet_id']?>"/>
             <input type="submit" formnovalidate="true" value="Modifier" onclick="if(window.confirm('Voulez-vous vraiment modifier le billet ?')){return true;}else{return false;}" />
         </form>
